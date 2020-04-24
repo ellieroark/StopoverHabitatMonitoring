ce25c714efbbef7968239a0378f13511279394e7
Ellie Roark <ellieroark@gmail.com>
2020-04-24 18:00:31 +0100

    added final species richness plot-- observed species detected for all four count types, and fitted GLMM predictions for all four count types.

--- plots_yellowpaper_PtAbbaye2019.R
+++ plots_yellowpaper_PtAbbaye2019.R
@@ -26,7 +26,7 @@ library(tidyverse)
 library(Hmisc)
 library(lme4)
 
-
+t_size <- 10
 #setwd("/home/emer/Dropbox/Ellie Roark/R/PointAbbaye/")
 
 ### clean up environment
@@ -774,166 +774,6 @@ ggplot(data = wiwr_aru10c_testpreds_gam, aes(x = OOB_preds, y = meandet)) +
 
 
 #### SUMMARY PLOTS FOR SPECIES RICHNESS GLMM------------------------------------
-##boxplot for sp detected by each count type
-count_type_box <- ggplot(spdet_paired, aes(x=count_type, y=sp_detected)) + 
-  geom_boxplot() + scale_y_continuous(breaks = c(1, 5, 10)) +
-  labs(x = "Count type", y = "Number of species detected") +
-  theme_bw()
-count_type_box
-
-##species detected over time, stratified by count type, with plotted GLMM
-## prediction line, from maxt2.spdetmm model! formula: 
-## sp_detected ~ count_type + wind + rain + noise + day_of_yr_s +  
-## rain:count_type + day_of_yr_s:count_type + min_past_sun_s +  
-##  (1 | day_of_yr_s/point_id)
-#first, create new df with response and predictor variable columns
-mean_doy <- mean(spdet_all$day_of_yr)
-sd_doy_c <- sd(spdet_all$day_of_yr_c)
-aru.pred <- data.frame(day_of_yr = seq(min(spdet_all$day_of_yr), 
-                                        max(spdet_all$day_of_yr), by = 1))
-aru.pred$day_of_yr_c <- aru.pred$day_of_yr-mean_doy
-aru.pred$day_of_yr_s <- aru.pred$day_of_yr_c/sd_doy_c
-#ptct.pred <- data.frame(day_of_yr_s = spdet_all$day_of_yr_s)
-aru.pred$day_sq_s <- aru.pred$day_of_yr_s^2
-aru.pred$min_past_sun_s <- median(spdet_all$min_past_sun_s)
-aru.pred$count_type <- as.factor("aru")
-aru.pred$wind <- as.factor("0-1")
-aru.pred$rain <- as.factor("Dry")
-aru.pred$noise <- as.factor("0")
-aru.pred$point_id <- as.factor("FB1")
-#coerce factor variables to contain the same number of levels as the original 
-aru.pred$count_type <- factor(aru.pred$count_type, 
-                               levels = c("point", "aru"), 
-                               labels = c("point", "aru"))
-aru.pred$wind <- factor(aru.pred$wind, 
-                         levels = c("0-1", "2", "3+"),
-                         labels = c("0-1", "2", "3+"))
-aru.pred$rain <- factor(aru.pred$rain, 
-                         levels = c("Dry", "wet"),
-                         labels = c("Dry", "wet"))
-aru.pred$noise <- factor(aru.pred$noise, 
-                          levels = c("0", "1", ">2"), 
-                          labels = c("0", "1", ">2"))
-aru.pred$point_id <- factor(aru.pred$point_id, 
-                             levels = c("p1", "p2", "p3", "p4", "p5", "p6",
-                                        "p8", "p9", "p10", "p11", "p12", 
-                                        "p13","p14", "p15", "p17", "p18", 
-                                        "FB1", "FB2"),
-                             labels = c("p1", "p2", "p3", "p4", "p5", "p6",
-                                        "p8", "p9", "p10", "p11", "p12", 
-                                        "p13","p14", "p15", "p17", "p18", 
-                                        "FB1", "FB2"))
-
-
-
-#next, feed that new data into the predict function for lmer package
-#make a new col with predictions
-aru.pred$preds <- predict(r1.spdetmm, newdata = aru.pred, type = "response", 
-                          re.form = NA)
-
-
-## now do the same thing with pt cts
-# ptct.pred <- data.frame(day_of_yr_s = seq(-1.86, 1.61, by = 0.01))
-ptct.pred <- data.frame(day_of_yr = seq(min(spdet_all$day_of_yr), 
-                                        max(spdet_all$day_of_yr), by = 1))
-ptct.pred$day_of_yr_c <- ptct.pred$day_of_yr-mean_doy
-ptct.pred$day_of_yr_s <- ptct.pred$day_of_yr_c/sd_doy_c
-rm(mean_doy, sd_doy_c) # clean workspace
-
-#ptct.pred <- data.frame(day_of_yr_s = spdet_all$day_of_yr_s)
-ptct.pred$day_sq_s <- ptct.pred$day_of_yr_s^2
-ptct.pred$min_past_sun_s <- median(spdet_all$min_past_sun_s)
-ptct.pred$count_type <- as.factor("point")
-ptct.pred$wind <- as.factor("0-1")
-ptct.pred$rain <- as.factor("Dry")
-ptct.pred$noise <- as.factor("0")
-ptct.pred$point_id <- as.factor("FB1")
-#coerce factor variables to contain the same number of levels as the original 
-ptct.pred$count_type <- factor(ptct.pred$count_type, 
-                               levels = c("point", "aru"), 
-                               labels = c("point", "aru"))
-ptct.pred$wind <- factor(ptct.pred$wind, 
-                         levels = c("0-1", "2", "3+"),
-                         labels = c("0-1", "2", "3+"))
-ptct.pred$rain <- factor(ptct.pred$rain, 
-                         levels = c("Dry", "wet"),
-                         labels = c("Dry", "wet"))
-ptct.pred$noise <- factor(ptct.pred$noise, 
-                          levels = c("0", "1", ">2"), 
-                          labels = c("0", "1", ">2"))
-ptct.pred$point_id <- factor(ptct.pred$point_id, 
-                             levels = c("p1", "p2", "p3", "p4", "p5", "p6",
-                                        "p8", "p9", "p10", "p11", "p12", 
-                                        "p13","p14", "p15", "p17", "p18", 
-                                        "FB1", "FB2"),
-                             labels = c("p1", "p2", "p3", "p4", "p5", "p6",
-                                        "p8", "p9", "p10", "p11", "p12", 
-                                        "p13","p14", "p15", "p17", "p18", 
-                                        "FB1", "FB2"))
-
-#now make a new col with predictions
-ptct.pred$preds <- predict(r1.spdetmm, type = "response",
-                           newdata = ptct.pred, re.form = NA)
-
-#bind ptct.pred to aru.pred
-ptct.pred <- bind_rows(ptct.pred, aru.pred)
-
-spdet_all$fitval <- predict(r1.spdetmm) # this gets fitted values
-
-#plot of fitted values
-spdet_time_fitv <- ggplot(spdet_all, aes(x=day_of_yr_s, y=fitval, 
-                                        color=count_type)) + 
-  geom_point(shape=1) +
-  theme_bw() +
-  scale_colour_hue(h = c(0, 360), l=30) # Use a slightly darker palette than normal
-spdet_time_fitv
-
-
-#create scatterplot of sp detected over time with count type as color
-spdet_time_ptct <- ggplot(ptct.pred, aes(x=day_of_yr, y=preds, 
-                                         color=count_type, 
-                                         group = count_type)) + 
-  # geom_point(shape=1) +
-  geom_line() + 
-  geom_point(data = spdet_all,
-             aes(x = day_of_yr, y = sp_detected, color = count_type)) +
-  theme_bw() +
-  scale_colour_viridis_d() + 
-  scale_y_continuous(breaks = c(0, 4, 8, 12)) + 
-  ylab("Number of Species") +
-  xlab("Day of Year")
-spdet_time_ptct
-
-spdet_time_aru <- ggplot(aru.pred, aes(x=day_of_yr_s, y=preds, 
-                                         color=count_type)) + 
-  geom_point(shape=1) +
-  theme_bw() +
-  scale_colour_hue(h = c(0, 360), l=30) # Use a slightly darker palette than normal
-spdet_time_aru
-
-##test adding preds to original data frame
-spdet_all$predicted <- predict(r1.spdetmm, newdata = spdet_all,
-                               type = "response")
-
-## plot preds for original data
-spdet_time_pred <- ggplot(spdet_all, aes(x=day_of_yr, y=predicted, 
-                                         color=count_type)) + 
-  geom_point(shape=1) +
-  theme_bw() +
-  scale_colour_hue(h = c(0, 360), l=30) + # Use a slightly darker palette than normal
-  scale_y_continuous(breaks = c(0, 4, 8, 12))
-spdet_time_pred
-
-# get mean for daily predictions from each count type
-arupred <- spdet_all[which(spdet_all$count_type == "aru"), ]
-ptctpred <- spdet_all[which(spdet_all$count_type == "point"), ]
-
-# aggregate(ptctpred$predicted, by = list(ptctpred$date), FUN = mean)
-# #OR
-# arupred %>%
-#   group_by(day_of_yr) %>%
-#   summarise(mean(predicted))
-
 
 ## plot of coefficients for GLMM (10 min vs 10 min)
 ## example from wg:
@@ -986,5 +826,220 @@ print(ggplot(data = r1_df,
         ylim(-1, 1) +
         theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1)))
 
+
+
+
+##### ##species detected over time, stratified by count type, with plotted GLMM
+## prediction line, from max.rand.spdetmm model! formula: 
+## sp_detected ~ count_type + wind + rain + noise + day_of_yr_s +  
+## rain:count_type + day_of_yr_s:count_type 
+##  (1 | day_of_yr_s)
+#first, create new df with response and predictor variable columns
+mean_doy <- mean(spdet_4ct$day_of_yr)
+sd_doy_c <- sd(spdet_4ct$day_of_yr_c)
+aru10c.pred <- data.frame(day_of_yr = seq(min(spdet_4ct$day_of_yr), 
+                                       max(spdet_4ct$day_of_yr), by = 1))
+aru10c.pred$day_of_yr_c <- aru10c.pred$day_of_yr-mean_doy
+aru10c.pred$day_of_yr_s <- aru10c.pred$day_of_yr_c/sd_doy_c
+#ptct.pred <- data.frame(day_of_yr_s = spdet_all$day_of_yr_s)
+aru10c.pred$day_sq_s <- aru10c.pred$day_of_yr_s^2
+aru10c.pred$count_type <- as.factor("aru")
+aru10c.pred$wind <- as.factor("0-1")
+aru10c.pred$rain <- as.factor("Dry")
+aru10c.pred$noise <- as.factor("0")
+aru10c.pred$point_id <- as.factor("FB1")
+#coerce factor variables to contain the same number of levels as the original 
+aru10c.pred$count_type <- factor(aru10c.pred$count_type, 
+                              levels = c("point", "aru", "aru_10r", "aru_22r"), 
+                              labels = c("point", "aru", "aru_10r", "aru_22r"))
+aru10c.pred$wind <- factor(aru10c.pred$wind, 
+                        levels = c("0-1", "2", "3+"),
+                        labels = c("0-1", "2", "3+"))
+aru10c.pred$rain <- factor(aru10c.pred$rain, 
+                        levels = c("Dry", "wet"),
+                        labels = c("Dry", "wet"))
+aru10c.pred$noise <- factor(aru10c.pred$noise, 
+                         levels = c("0", "1", ">2"), 
+                         labels = c("0", "1", ">2"))
+aru10c.pred$point_id <- factor(aru10c.pred$point_id, 
+                            levels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                       "p8", "p9", "p10", "p11", "p12", 
+                                       "p13","p14", "p15", "p17", "p18", 
+                                       "FB1", "FB2"),
+                            labels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                       "p8", "p9", "p10", "p11", "p12", 
+                                       "p13","p14", "p15", "p17", "p18", 
+                                       "FB1", "FB2"))
+
+
+
+#next, feed that new data into the predict function for lmer package
+#make a new col with predictions
+aru10c.pred$preds <- predict(max.rand.spdetmm, newdata = aru10c.pred, 
+                             type = "response", re.form = NA)
+
+
+## now do the same thing with pt cts
+# ptct.pred <- data.frame(day_of_yr_s = seq(-1.86, 1.61, by = 0.01))
+ptct.pred <- data.frame(day_of_yr = seq(min(spdet_4ct$day_of_yr), 
+                                        max(spdet_4ct$day_of_yr), by = 1))
+ptct.pred$day_of_yr_c <- ptct.pred$day_of_yr-mean_doy
+ptct.pred$day_of_yr_s <- ptct.pred$day_of_yr_c/sd_doy_c
+
+#ptct.pred <- data.frame(day_of_yr_s = spdet_all$day_of_yr_s)
+ptct.pred$day_sq_s <- ptct.pred$day_of_yr_s^2
+ptct.pred$count_type <- as.factor("point")
+ptct.pred$wind <- as.factor("0-1")
+ptct.pred$rain <- as.factor("Dry")
+ptct.pred$noise <- as.factor("0")
+ptct.pred$point_id <- as.factor("FB1")
+#coerce factor variables to contain the same number of levels as the original 
+ptct.pred$count_type <- factor(ptct.pred$count_type, 
+                               levels = c("point", "aru", "aru_10r", "aru_22r"), 
+                               labels = c("point", "aru", "aru_10r", "aru_22r"))
+ptct.pred$wind <- factor(ptct.pred$wind, 
+                         levels = c("0-1", "2", "3+"),
+                         labels = c("0-1", "2", "3+"))
+ptct.pred$rain <- factor(ptct.pred$rain, 
+                         levels = c("Dry", "wet"),
+                         labels = c("Dry", "wet"))
+ptct.pred$noise <- factor(ptct.pred$noise, 
+                          levels = c("0", "1", ">2"), 
+                          labels = c("0", "1", ">2"))
+ptct.pred$point_id <- factor(ptct.pred$point_id, 
+                             levels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                        "p8", "p9", "p10", "p11", "p12", 
+                                        "p13","p14", "p15", "p17", "p18", 
+                                        "FB1", "FB2"),
+                             labels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                        "p8", "p9", "p10", "p11", "p12", 
+                                        "p13","p14", "p15", "p17", "p18", 
+                                        "FB1", "FB2"))
+
+#now make a new col with predictions
+ptct.pred$preds <- predict(max.rand.spdetmm, type = "response",
+                           newdata = ptct.pred, re.form = NA)
+
+## now do the same thing with aru10rand
+aru10r.pred <- data.frame(day_of_yr = seq(min(spdet_4ct$day_of_yr), 
+                                        max(spdet_4ct$day_of_yr), by = 1))
+aru10r.pred$day_of_yr_c <- aru10r.pred$day_of_yr-mean_doy
+aru10r.pred$day_of_yr_s <- aru10r.pred$day_of_yr_c/sd_doy_c
+aru10r.pred$day_sq_s <- aru10r.pred$day_of_yr_s^2
+aru10r.pred$count_type <- as.factor("aru_10r")
+aru10r.pred$wind <- as.factor("0-1")
+aru10r.pred$rain <- as.factor("Dry")
+aru10r.pred$noise <- as.factor("0")
+aru10r.pred$point_id <- as.factor("FB1")
+#coerce factor variables to contain the same number of levels as the original 
+aru10r.pred$count_type <- factor(aru10r.pred$count_type, 
+                               levels = c("point", "aru", "aru_10r", "aru_20r"), 
+                               labels = c("point", "aru", "aru_10r", "aru_20r"))
+aru10r.pred$wind <- factor(aru10r.pred$wind, 
+                         levels = c("0-1", "2", "3+"),
+                         labels = c("0-1", "2", "3+"))
+aru10r.pred$rain <- factor(aru10r.pred$rain, 
+                         levels = c("Dry", "wet"),
+                         labels = c("Dry", "wet"))
+aru10r.pred$noise <- factor(aru10r.pred$noise, 
+                          levels = c("0", "1", ">2"), 
+                          labels = c("0", "1", ">2"))
+aru10r.pred$point_id <- factor(aru10r.pred$point_id, 
+                             levels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                        "p8", "p9", "p10", "p11", "p12", 
+                                        "p13","p14", "p15", "p17", "p18", 
+                                        "FB1", "FB2"),
+                             labels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                        "p8", "p9", "p10", "p11", "p12", 
+                                        "p13","p14", "p15", "p17", "p18", 
+                                        "FB1", "FB2"))
+
+#now make a new col with predictions
+aru10r.pred$preds <- predict(max.rand.spdetmm, type = "response",
+                           newdata = aru10r.pred, re.form = NA)
+
+## now do the same thing with aru22rand
+aru22r.pred <- data.frame(day_of_yr = seq(min(spdet_4ct$day_of_yr), 
+                                          max(spdet_4ct$day_of_yr), by = 1))
+aru22r.pred$day_of_yr_c <- aru22r.pred$day_of_yr-mean_doy
+aru22r.pred$day_of_yr_s <- aru22r.pred$day_of_yr_c/sd_doy_c
+
+aru22r.pred$day_sq_s <- aru22r.pred$day_of_yr_s^2
+aru22r.pred$count_type <- as.factor("aru_22r")
+aru22r.pred$wind <- as.factor("0-1")
+aru22r.pred$rain <- as.factor("Dry")
+aru22r.pred$noise <- as.factor("0")
+aru22r.pred$point_id <- as.factor("FB1")
+#coerce factor variables to contain the same number of levels as the original 
+aru22r.pred$count_type <- factor(aru22r.pred$count_type, 
+                                 levels = c("point", "aru", "aru_10r", "aru_22r"), 
+                                 labels = c("point", "aru", "aru_10r", "aru_22r"))
+aru22r.pred$wind <- factor(aru22r.pred$wind, 
+                           levels = c("0-1", "2", "3+"),
+                           labels = c("0-1", "2", "3+"))
+aru22r.pred$rain <- factor(aru22r.pred$rain, 
+                           levels = c("Dry", "wet"),
+                           labels = c("Dry", "wet"))
+aru22r.pred$noise <- factor(aru22r.pred$noise, 
+                            levels = c("0", "1", ">2"), 
+                            labels = c("0", "1", ">2"))
+aru22r.pred$point_id <- factor(aru22r.pred$point_id, 
+                               levels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                          "p8", "p9", "p10", "p11", "p12", 
+                                          "p13","p14", "p15", "p17", "p18", 
+                                          "FB1", "FB2"),
+                               labels = c("p1", "p2", "p3", "p4", "p5", "p6",
+                                          "p8", "p9", "p10", "p11", "p12", 
+                                          "p13","p14", "p15", "p17", "p18", 
+                                          "FB1", "FB2"))
+
+#now make a new col with predictions
+aru22r.pred$preds <- predict(max.rand.spdetmm, type = "response",
+                             newdata = aru22r.pred, re.form = NA)
+
+#bind ptct.pred to aru10c.pred to aru10r.pred to aru22r.pred
+pred.4ct <- bind_rows(ptct.pred, aru10c.pred, aru10r.pred, aru22r.pred)
+spdet_time <- ggplot(pred.4ct, 
+                     aes(x=day_of_yr, y=preds, 
+                         color= factor(count_type, 
+                                       levels = c("point", "aru", 
+                                                  "aru_10r", "aru_22r"), 
+                                       labels = c("Point count\n", 
+                                                  "ARU - 10 min\nconsecutive\n", 
+                                                  "ARU - 10 min\nrandom\n", 
+                                                  "ARU - 22 min\nrandom\n")), 
+                         group = factor(count_type, 
+                                        levels = c("point", "aru", 
+                                                   "aru_10r", "aru_22r"), 
+                                        labels = c("Point count\n", 
+                                                   "ARU - 10 min\nconsecutive\n", 
+                                                   "ARU - 10 min\nrandom\n", 
+                                                   "ARU - 22 min\nrandom\n")))) + 
+  # geom_point(shape=1) +
+  geom_line(size = t_size/10) + 
+  geom_point(data = spdet_4ct,
+             aes(x = day_of_yr, y = sp_detected, 
+                 color = factor(count_type, 
+                                levels = c("point", "aru", 
+                                           "aru_10r", "aru_22r"), 
+                                labels = c("Point count\n", 
+                                           "ARU - 10 min\nconsecutive\n", 
+                                           "ARU - 10 min\nrandom\n", 
+                                           "ARU - 22 min\nrandom\n")))) +
+  scale_colour_viridis_d(name = "Survey Method", option = "magma", 
+                         begin = 0, end = 0.75) + 
+  scale_y_continuous(breaks = c(0, 4, 8, 12, 16)) + 
+  ylab("Number of Species") +
+  xlab("Day of Year") + 
+  theme_bw() + 
+  theme(text = element_text(size = t_size))
+  
+spdet_time
+
 #### END summary plots for species richness GLMM-------------------------------
 
+
+### write out plots as jpgs ---------------------------------------------------
+ggsave(spdet_time, filename = "./saved_objects/spdet_time.jpg", 
+       width = 10, height = 6, 
+       units = "cm", device = "jpeg")
